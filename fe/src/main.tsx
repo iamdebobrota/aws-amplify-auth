2d23cba30fbab4ecbd3dbddb26ccaaaa6a6e8951
iamdebobrota <iamdebobrota@gmail.com>
2024-11-20 14:00:42 +0530

    Add amplify

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
+// import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
